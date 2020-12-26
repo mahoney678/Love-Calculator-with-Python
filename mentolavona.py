1d402490c35eaf1ab0967f2aa12125bb12673987
Mert Sırakaya <62549656+fswair@users.noreply.github.com>
2020-12-26 13:12:38 +0300

    Add files via upload

---
+++ mentolavona.py
@@ -0,0 +1,65 @@
+import time
+'''
+
+a = input("Telefon numaranın son 4 rakamını gir..:")
+
+b = input("Biriciğinizin, Telefon numarasının son 4 rakamını gir..:")
+'''
+
+a = input("Telefon numaranı giriniz..:")
+
+b = input("Biriciğinizin, Telefon numarasını giriniz..:")
+c = []
+def __init__():
+    c.append(a[0])
+    c.append(a[1])
+    c.append(a[2])
+    c.append(a[3])
+    c.append(a[4])
+    c.append(a[5])
+    c.append(a[6])
+    c.append(a[7])
+    c.append(a[8])
+    c.append(a[9])
+    c.append(a[10])
+    c.append(b[0])
+    c.append(b[1])
+    c.append(b[2])
+    c.append(b[3])
+    c.append(b[4])
+    c.append(b[5])
+    c.append(b[6])
+    c.append(b[7])
+    c.append(b[8])
+    c.append(b[9])
+    c.append(b[10])
+    c[0] = int(c[0])
+    c[1] = int(c[1])
+    c[2] = int(c[2])
+    c[3] = int(c[3])
+    c[4] = int(c[4])
+    c[5] = int(c[5])
+    c[6] = int(c[6])
+    c[7] = int(c[7])
+    c[8] = int(c[8])
+    c[9] = int(c[9])
+    c[10] = int(c[10])
+    c[11] = int(c[11])
+    c[12] = int(c[12])
+    c[13] = int(c[13])
+    c[14] = int(c[14])
+    c[15] = int(c[15])
+    c[16] = int(c[16])
+    c[17] = int(c[17])
+    c[18] = int(c[18])
+    c[19] = int(c[19])
+    c[20] = int(c[20])
+    c[21] = int(c[21])
+    x = sum(c)
+    print("Aşk puanı 50/", int(x / 2), "  kere maşallah! & yani %", x)
+
+__init__()
+
+
+
+time.sleep(50)
